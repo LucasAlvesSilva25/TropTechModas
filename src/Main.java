d762851b8776e4979955f124c87b4cf72b68ce44
lucas <lucas3668@gmail.com>
2026-02-01 02:03:29 -0300

    Hoje consegui fazer a lógica para chamar o ClienteService na camada de View, sem que essa tenha acessa as camadas model e repository.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
+import Service.ClienteService;
+
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Tipo (PF/PJ): ");
+        String tipo = scanner.nextLine();
+
+        System.out.print("Nome/Razão Social: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Telefone: ");
+        String telefone = scanner.nextLine();
+
+        System.out.print("CPF/CNPJ: ");
+        String documento = scanner.nextLine();
+
+        System.out.print("Rua: ");
+        String rua = scanner.nextLine();
+
+        System.out.print("Numero: ");
+        String numero = scanner.nextLine();
+
+        System.out.print("Cidade: ");
+        String cidade = scanner.nextLine();
+
+        System.out.print("Estado: ");
+        String estado = scanner.nextLine();
+
+        System.out.print("País: ");
+        String pais = scanner.nextLine();
+
+
+
+        ClienteService clienteService = new ClienteService();
+        String cadastroClient = clienteService.cadastrarCliente(tipo,nome,telefone,documento,rua,numero,cidade,estado,pais);
+
+        System.out.println("Retorno: " + cadastroClient);
 
     }
 }
--- src/Service/ClienteService.java
+++ src/Service/ClienteService.java
@@ -14,27 +14,47 @@ public class ClienteService {
     public ClienteService() {
         this.pessoaFisicaRepo = new PessoaFisicaRepository();
         this.pessoaJuridicaRepo = new PessoaJuridicaRepository();
-    }
 
-    public void cadastrarCliente(){
-
+        // preciso verificar se vai ser necessário isso
     }
 
     public Endereco cadastrarEndereco(String rua, String numero, String cidade, String estado, String pais){
         return new Endereco(rua, numero, cidade, estado, pais);
     }
 
-//    private String cadastrarPessoaFisica(String nome, String telefone, ) {
-//
-//        if (pessoaFisicaRepo.buscarPorCpf(cpf) != null) {
-//            throw new IllegalArgumentException("CPF já cadastrado");
+
+    public String cadastrarCliente(String tipo, String nome, String telefone,String documento, String rua, String numero, String cidade, String estado, String pais){
+
+        // vou chamar esse metódo na main passando as informações  e ele vai chamar os metodos responsaveis por fazer o cadastro do PF ou PJ
+
+        Endereco endereco = cadastrarEndereco(rua,numero,cidade,estado,pais);
+
+        if ("PF".equalsIgnoreCase(tipo)) {
+
+            return cadastrarPessoaFisica(nome,telefone,endereco,documento);
+
+       }
+//        else if ("PJ".equalsIgnoreCase(tipo)) {
+//            cliente = new PessoaJuridica(nome, telefone, endereco, documento, email);
+//            pessoaJuridicaRepo.adicionar((PessoaJuridica) cliente);
 //        }
-//
-//        PessoaFisica pf = new PessoaFisica(null, nome, cpf, email, telefone);
-//        pessoaFisicaRepo.adicionar(pf);
-//        return "Pessoa Física cadastrada com sucesso!";
-//    }
-//
+
+            return null;
+    }
+
+    private String cadastrarPessoaFisica(String nome, String telefone,
+                                         Endereco endereco, String cpf) {
+
+        if (pessoaFisicaRepo.buscarPorCpf(cpf) != null) {
+            throw new IllegalArgumentException("CPF já cadastrado");
+        }
+
+        PessoaFisica pf = new PessoaFisica(nome,telefone,endereco,cpf);
+        pessoaFisicaRepo.adicionar(pf);
+        return "Pessoa Física cadastrada com sucesso!";
+    }
+
+    // Acrescentar a mesma logica do PF e colocar no cadastrar Client
 //    private String cadastrarPessoaJuridica(String razaoSocial, String cnpj,
 //                                           String email, String telefone) {
 //        // Valida CNPJ específico
